7a38d5b48d0de553d92f6163103e1b6449cc7f7b
Finsby1 <bej002@edu.zealand.dk>
2025-03-06 19:26:27 +0100

    asdasdasd

---
+++ Trophies/Trophy.cs
@@ -0,0 +1,57 @@
+﻿namespace Trophies;
+
+public class Trophy
+{
+    private string _competition;
+    private int _year;
+
+    public int Id { get; set; }
+
+    public string Competition
+    {
+        get => _competition;
+        set
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("Competition cannot be null.");
+            }
+
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Competition must be at least 3 characters long.");
+            }
+            _competition = value;
+        }
+    }
+
+    public int Year
+    {
+        get => _year;
+        set
+        {
+            if (value < 1970 || value > DateTime.Now.Year)
+            {
+                throw new ArgumentException($"Year must be between 1970 and {DateTime.Now.Year}.");
+            }
+        }
+    }
+
+    public Trophy(int id, string competition, int year)
+    {
+        Id = id;
+        Competition = competition;
+        Year = year;
+    }
+
+    public Trophy()
+    {
+        
+    }
+
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Competition: {Competition}, Year: {Year}";
+    }
+}
---
+++ TrophyTest/TrophyTests.cs
@@ -0,0 +1,15 @@
+using Trophies;
+
+namespace TrophyTest;
+
+[TestClass]
+public class TrophyTests
+{
+    private readonly Trophy _trophy = new Trophy(1, "Swimming", 2010);
+    
+    [TestMethod]
+    public void TrophyTest1()
+    {
+        
+    }
+}
